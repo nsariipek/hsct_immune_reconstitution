b9f5b305bf6830c07c1fcec87c7976b7abc38008
petervangalen <petervangalen@gmail.com>
2025-02-10 00:08:54 +0000

    Initial analysis of TCAT programs using starCAT

---
+++ 3_DGE/3.2_PvG-starCAT.sh
@@ -0,0 +1,16 @@
+# Peter van Galen, 250209
+# Quantify T cell programs using starCAT (https://www.biorxiv.org/content/10.1101/2024.05.03.592310v1, https://github.com/immunogenomics/starCAT/blob/main/Examples/starCAT_vignette_R.ipynb)  
+# This should be run after creating matrix.mtx, barcodes.tsv, and features.tsv as described in 3.2_PvG-TCAT.R
+
+# Installation (only run once)
+#sudo apt install python3-pip
+#pip install starcatpy
+# Add to ~/.bashrc
+#export PATH=$HOME/.local/bin:$PATH
+
+# Determine program usage 
+starcat --reference "TCAT.V1" \
+        --counts "/home/unix/vangalen/TP53_ImmuneEscape/3_DGE/starcat/matrix.mtx.gz" \
+        --output-dir "/home/unix/vangalen/TP53_ImmuneEscape/3_DGE/starcat/" \
+        --name "results"
+

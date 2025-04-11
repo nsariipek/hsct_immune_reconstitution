c5e1cbffb0f598dbb2d15f8cb229db82fb95cfc3
nsariipek <n.eminesariipek@gmail.com>
2025-04-11 11:25:45 -0400

    numbat
    numbat visualization

--- 8_Numbat/8.3_Visualize_Numbat_results.R
+++ 8_Numbat/8.3_Visualize_Numbat_results.R
@@ -1,7 +1,8 @@
 # Peter van Galen, 241101
+# Updated by Nurefsan Sariipek, 250411
 # Visualize one of the Numbat results files 
-# This code has not been updated to work in this location
 
+# Better to run this script on the local space rather than Terra
 library(tidyverse)
 library(igraph)
 library(numbat)
@@ -15,7 +16,7 @@ library(patchwork)
 rm(list=ls())
 
 # For Nurefsan:
-setwd("~/TP53_ImmuneEscape/5_Cell_Proportions/")
+setwd("/Users/dz855/Partners HealthCare Dropbox/Nurefsan Sariipek/ImmuneEscapeTP53/TP53_ImmuneEscape/8_Numbat")
 
 # Favorite function
 cutf <- function(x, f=1, d="/") sapply(strsplit(x, d), function(i) paste(i[f], collapse=d))
@@ -24,15 +25,46 @@ cutf <- function(x, f=1, d="/") sapply(strsplit(x, d), function(i) paste(i[f], c
 #nb <- readRDS("~/Library/Mobile Documents/com~apple~CloudDocs/Downloads/nb_TNBC1.rds")
 
 # Load numbat object from our own data (need to mount Broad storage first)
-nb = Numbat$new(out_dir = "/Volumes/broad_vangalenlab/sariipek/numbat/P05")
+#For Nurefsan
+nb = Numbat$new(out_dir = "/Volumes/sariipek/numbat/old/P08")
+pagoda = readRDS(url('http://pklab.org/teng/data/con_TNBC1.rds'))
+
 
 # Generate CNV plot
 mypal = c('1' = 'gray', '2' = "#377EB8", '3' = "#4DAF4A", '4' = "#984EA3")
 
-nb$plot_phylo_heatmap(
+plot1 <- nb$plot_phylo_heatmap(
   clone_bar = TRUE, 
   p_min = 0.9,
   pal_clone = mypal
 )
+#Save the panel as pdf
+plot1
+pdf("P08_panel2.pdf", width=15, height=5)
+plot1
+dev.off()
+
+plots = lapply(
+  1:4,
+  function(k) {
+    nb$cutree(n_cut = k)
+    nb$plot_phylo_heatmap() + ggtitle(paste0('n_cut=', k))
+  }
+)
+wrap_plots(plots)
+
+# Visualize  CNV events in pseudobulks where the data is more rich, aggregating cells by clone
+plot2 <- nb$bulk_clones %>% 
+  filter(n_cells > 50) %>%
+  plot_bulks(
+    min_LLR = 10, # filtering CNVs by evidence
+    legend = TRUE
+  )
+plot2
+#Save as pdf
+pdf("P08_bulk_clones.pdf", width=15, height=10)
+plot2
+dev.off()
+
 
 

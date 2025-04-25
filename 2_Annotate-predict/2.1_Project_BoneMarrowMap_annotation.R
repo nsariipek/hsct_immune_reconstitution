31afaa69fb2a262ca070beac023b306d0fb048aa
nsariipek <n.eminesariipek@gmail.com>
2025-04-25 16:50:51 +0000

    update on the script

--- 2_Annotate-predict/2.1_Project_BoneMarrowMap_annotation.R
+++ 2_Annotate-predict/2.1_Project_BoneMarrowMap_annotation.R
@@ -8,16 +8,20 @@
   library(RColorBrewer)
   library(patchwork)
   
-# For BM library see this :  
+# For running the BM library first time run this :  
+  # ## install dependencies that are not on CRAN
+  # if(!require(BiocManager, quietly = TRUE)) install.packages("BiocManager")
+  # BiocManager::install(c("AUCell", "doMC", "BiocNeighbors"))
+  # if(!require(devtools, quietly = TRUE)) install.packages("devtools")
+  # devtools::install_github("jaredhuling/jcolors")
+  # ## install BoneMarrowMap package
+  # devtools::install_github('andygxzeng/BoneMarrowMap', force = TRUE)
 
 # Set working directory
 setwd("~/TP53_ImmuneEscape/2_Annotate-predict/")
 
-base_dir = "/Users/kr72/Documents/projects/aging_blood/prepare_for_publication/"
-
+# Load the object that Peter advised to use
 seu = readRDS("~/250417_MergedSeuratObject.rds")
-samples = seu$orig.ident %>% unique()
-
 
 # Set directory to store projection reference files
 projection_path = '~/'
@@ -34,13 +38,13 @@ ref <- readRDS(paste0(projection_path, 'BoneMarrowMap_SymphonyReference.rds'))
 # Set uwot path for UMAP projection
 ref$save_uwot_path <- paste0(projection_path, 'BoneMarrowMap_uwot_model.uwot')
 
-ReferenceSeuratObj <- create_ReferenceObject(ref)
-DimPlot(ReferenceSeuratObj, reduction = 'umap', group.by = 'CellType_Annotation_formatted', 
-        raster=FALSE, label=TRUE, label.size = 4) + NoAxes()
+# ReferenceSeuratObj <- create_ReferenceObject(ref)
+# DimPlot(ReferenceSeuratObj, reduction = 'umap', group.by = 'CellType_Annotation_formatted', 
+#         raster=FALSE, label=TRUE, label.size = 4) + NoAxes()
 
 ### Map each of the samples onto BMM reference; this follows BMM tutorial ####
-
 samples <- unique(seu$orig.ident)
+
 for (i in 1:length(samples)) {
   
   query = subset(seu, orig.ident==samples[i])
@@ -84,14 +88,15 @@ for (i in 1:length(annotation_files)) {
 bmm_annotations = bmm_annotations[colnames(seu), ]
 all(colnames(seu) == rownames(bmm_annotations))
 
-#just select the needed columns and saved them for later 
+# Select the needed columns and saved them as a tsv
 bmm_annotations = bmm_annotations[,c(16:23)]
 
 write.table(bmm_annotations, "bmm_annotations.tsv", append = F, quote = F, sep = "\t", row.names = T, col.names = T)
 
+# Add into your seurat object
 seu = AddMetaData(seu, bmm_annotations)
 
-# take out the UMAP coordinates
+# Take out the UMAP coordinates
 umap_coords = as.matrix(seu@meta.data[, c("umap_1", "umap_2")])
 
 # Ensure row names match the Seurat object cell names
